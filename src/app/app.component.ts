274ff19c49cd104728d640b9e3b789914f0646f8
Deep Saha <deepsaha288@yahoo.com>
2020-09-19 13:19:14 +0530

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,53 @@
+import { Component } from '@angular/core';
+import { CommonService } from './common.service';
+import { from } from 'rxjs';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'CRUDEXAMPLE';
+  allUser: Object;
+  isEdit=false;
+  userObj={
+    name:'',
+    mobile:'',
+    email:'',
+    password:'',
+    id:''
+  }
+  constructor(private commonService:CommonService){}
+  
+   ngOnInit(){
+     this.getLatestUser();
+   }
+  addUser(formObj)
+  {
+    console.log(formObj)
+    this.commonService.createUser(formObj).subscribe((response)=>{
+      this.getLatestUser();
+    })
+  }
+  getLatestUser(){
+    this.commonService.getAllUser().subscribe((response)=>{
+      this.allUser =response
+    })
+  }
+  editUser(user){
+    this.isEdit = true;
+    this.userObj = user;
+  }
+  deleteUser(user){
+    this.commonService.deleteUser(user).subscribe(()=>{
+    this.getLatestUser();
+    })
+  }
+  updateUser(){
+    this.isEdit = !this.isEdit;
+    this.commonService.updateUser(this.userObj).subscribe(()=>{
+     this.getLatestUser();
+    })
+  }
+}
